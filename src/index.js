ef00d70af1d436e672385dfe8825a2c75bdb8c0f
boozilla <io.async@gmail.com>
2025-10-02 00:31:20 +0900

    configuration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import { API } from 'homebridge';
 import { PLATFORM_NAME } from './settings.js';
 import { ShomePlatform } from './platform.js';
 
-export = (api: API) => {
+export default (api: API) => {
     api.registerPlatform(PLATFORM_NAME, ShomePlatform);
 };
